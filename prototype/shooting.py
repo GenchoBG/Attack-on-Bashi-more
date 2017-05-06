2f66b7d85de4d54402857956fca416504a675dfb
GenchoBG <genchobg11@gmail.com>
2017-05-06 21:35:39 +0300

    Added speeding up of the enemys spawning of new enemys and ending the game when you get caught

--- prototype/shooting.py
+++ prototype/shooting.py
@@ -1,4 +1,4 @@
-import pygame, sys, random, threading
+import pygame, sys, random, threading, math
 from pygame.locals import *
 
 bif = "bg.jpg"
@@ -11,13 +11,18 @@ background = pygame.image.load(bif).convert()
 bashi = pygame.image.load(bashiImage).convert_alpha()
 pepe = pygame.image.load(pepe).convert_alpha()
 
-myfont = pygame.font.SysFont("monospace", 15)
+myfont = pygame.font.SysFont("monospace", 18)
 
 word = ""
 enemys = []
 words = ["jica", "zona", "tokonositel", "pn prehod", "tranzistor", "diod", "popravka"]
-
+speed = 2
 images = [pepe]
+lost = False
+
+class Bashi:
+    x = 250 - bashi.get_rect().size[0] / 2
+    y = 500 - bashi.get_rect().size[1]
 
 
 class Enemy:
@@ -29,9 +34,14 @@ class Enemy:
         self.image = images[random.randint(0, len(images) - 1)]
 
 
-for i in range(0, 10):
-    enemys.append(Enemy())
 
+
+def GenerateEnemys():
+    threading.Timer(5, GenerateEnemys).start()
+    for i in range(0, 7):
+        enemys.append(Enemy())
+
+GenerateEnemys()
 
 def ToKillOrNotToKill():
     for enemy in enemys:
@@ -44,28 +54,67 @@ def Kill():
     for enemy in enemys:
         if enemy.word == word:
             enemys.remove(enemy)
-            print("KILL")
             return
+
+
+def Move():
+    threading.Timer(0.1, Move).start()
+    for enemy in enemys:
+        if (enemy.x < Bashi.x):
+            enemy.x += speed
+        elif (enemy.x > Bashi.x):
+            enemy.x -= speed
+        if (enemy.y < Bashi.y):
+            enemy.y += speed
+
+
+Move()
+
+
+def SpeedUp():
+    global speed
+    threading.Timer(1, SpeedUp).start()
+    speed += 0.1
+
+SpeedUp()
+
+def CheckForRIP():
+    global Move
+    global GenerateEnemys
+    global SpeedUp
+    for enemy in enemys:
+        if(enemy.x - Bashi.x < speed + 1 and Bashi.y - enemy.y < 1):
+            Move = None
+            GenerateEnemys = None
+            SpeedUp = None
+            return True
 
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+            GenerateEnemys = None
+            SpeedUp = None
+            Move = None
             sys.exit()
         if event.type == MOUSEBUTTONDOWN:
             for enemy in enemys:
                 print("--" + enemy.word)
         if event.type == KEYDOWN:
-            if (event.key) == 13:
+            if event.key == 13:
                 while ToKillOrNotToKill():
                     Kill()
                 word = ""
+            elif event.key == 8:
+                word = ''.join([word[x] for x in range(0, len(word)-1)])
             else:
                 word += chr(event.key)
 
+
+
     screen.blit(background, (0, 0))
-    screen.blit(bashi, (250 - bashi.get_rect().size[0] / 2, 500 - bashi.get_rect().size[1]))
+    screen.blit(bashi, (Bashi.x, Bashi.y))
     label = myfont.render(word, 1, (255, 255, 0))
     screen.blit(label, (0, 0))
 
@@ -73,5 +122,13 @@ while True:
         screen.blit(enemy.image, (enemy.x, enemy.y))
         curLabel = myfont.render(enemy.word, 1, (0, 0, 0))
         screen.blit(curLabel, (enemy.x, enemy.y))
+
+    if (not lost) and CheckForRIP():
+        lost = True
+
+    if(lost):
+        enemys = []
+        loseLabel = myfont.render("U LOSE", 1, (255, 0, 0))
+        screen.blit(loseLabel, (200, 250))
 
     pygame.display.update()
