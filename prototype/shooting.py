16b1df9fc5d760f3a2d58fd35187921c14a95936
GenchoBG <genchobg11@gmail.com>
2017-05-06 21:50:10 +0300

    Added scoring and highscore storing

--- prototype/shooting.py
+++ prototype/shooting.py
@@ -16,9 +16,14 @@ myfont = pygame.font.SysFont("monospace", 18)
 word = ""
 enemys = []
 words = ["jica", "zona", "tokonositel", "pn prehod", "tranzistor", "diod", "popravka"]
-speed = 2
+speed = 1.5
 images = [pepe]
 lost = False
+highscore = 0
+with open("highscore.txt", "r") as f: highscore = int(f.read())
+score = 0
+
+
 
 class Bashi:
     x = 250 - bashi.get_rect().size[0] / 2
@@ -34,14 +39,14 @@ class Enemy:
         self.image = images[random.randint(0, len(images) - 1)]
 
 
-
-
 def GenerateEnemys():
     threading.Timer(5, GenerateEnemys).start()
     for i in range(0, 7):
         enemys.append(Enemy())
 
+
 GenerateEnemys()
+
 
 def ToKillOrNotToKill():
     for enemy in enemys:
@@ -51,9 +56,11 @@ def ToKillOrNotToKill():
 
 
 def Kill():
+    global score
     for enemy in enemys:
         if enemy.word == word:
             enemys.remove(enemy)
+            score += 1
             return
 
 
@@ -76,14 +83,16 @@ def SpeedUp():
     threading.Timer(1, SpeedUp).start()
     speed += 0.1
 
+
 SpeedUp()
+
 
 def CheckForRIP():
     global Move
     global GenerateEnemys
     global SpeedUp
     for enemy in enemys:
-        if(enemy.x - Bashi.x < speed + 1 and Bashi.y - enemy.y < 1):
+        if (enemy.x - Bashi.x < speed + 1 and Bashi.y - enemy.y < 1):
             Move = None
             GenerateEnemys = None
             SpeedUp = None
@@ -107,16 +116,17 @@ while True:
                     Kill()
                 word = ""
             elif event.key == 8:
-                word = ''.join([word[x] for x in range(0, len(word)-1)])
+                word = ''.join([word[x] for x in range(0, len(word) - 1)])
             else:
                 word += chr(event.key)
 
-
-
     screen.blit(background, (0, 0))
     screen.blit(bashi, (Bashi.x, Bashi.y))
-    label = myfont.render(word, 1, (255, 255, 0))
-    screen.blit(label, (0, 0))
+    scorelabel = myfont.render("Score: " + str(score), 1, (255, 255, 0))
+    screen.blit(scorelabel, (0, 0))
+    wordlabel = myfont.render("Word: " + word, 1, (255, 255, 0))
+    screen.blit(wordlabel, (0, 21))
+
 
     for enemy in enemys:
         screen.blit(enemy.image, (enemy.x, enemy.y))
@@ -126,9 +136,13 @@ while True:
     if (not lost) and CheckForRIP():
         lost = True
 
-    if(lost):
+    if (lost):
         enemys = []
-        loseLabel = myfont.render("U LOSE", 1, (255, 0, 0))
-        screen.blit(loseLabel, (200, 250))
+        loseLabel = myfont.render("U LOSE. SCORE = " + str(score), 1, (255, 0, 0))
+        screen.blit(loseLabel, (175, 250))
+        if(score>highscore):
+            with open("highscore.txt", "w") as f: f.write(str(score))
+            highscorelabel = myfont.render("NEW HIGHSCORE!", 1, (255, 0, 0))
+            screen.blit(highscorelabel, (200, 271))
 
     pygame.display.update()
